3816371bfbd0260400484e7080413d7b006beb82
yukiasai <yukiasai@gmail.com>
2015-12-14 12:31:55 +0900

    Write source's test.

--- SoySourceTests/SoySourceTests.swift
+++ SoySourceTests/SoySourceTests.swift
@@ -13,24 +13,82 @@ class SoySourceTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddSection() {
+        let source = SoySource().addSection(Section())
+        XCTAssertEqual(source.sections.count, 1)
+        
+        source.addSection(Section())
+        source.addSection(Section())
+        XCTAssertEqual(source.sections.count, 3)
+        
+        // Method chain
+        source.addSection(Section()).addSection(Section()).addSection(Section())
+        XCTAssertEqual(source.sections.count, 6)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testAddSections() {
+        let source = SoySource().addSections([Section(), Section()])
+        XCTAssertEqual(source.sections.count, 2)
+        
+        // Method chain
+        source.addSections([Section()]).addSections([Section(), Section()])
+        XCTAssertEqual(source.sections.count, 5)
+    }
+    
+    func testCreateSection() {
+        let source = SoySource().createSection { _ in }
+        XCTAssertEqual(source.sections.count, 1)
+        
+        source.createSection { _ in }
+        source.createSection { _ in }
+        XCTAssertEqual(source.sections.count, 3)
+        
+        // Method chain
+        source.createSection { _ in }.createSection { _ in }
+        XCTAssertEqual(source.sections.count, 5)
+    }
+    
+    func testCreateSections() {
+        let source = SoySource()
+        
+        // Count
+        let count = UInt(2)
+        source.createSections(count) { _ in }
+        XCTAssertEqual(source.sections.count, Int(count))
+        
+        // Items
+        let items = [1, 2, 3]
+        source.createSections(items) { _ in }
+        XCTAssertEqual(source.sections.count, Int(count) + items.count)
+    }
+    
+    func testSectionAndRowAtIndex() {
+        let source = SoySource()
+        
+        let section1 = Section()
+        let row1_1 = Row()
+        let row1_2 = Row()
+        section1.addRows([row1_1, row1_2])
+        
+        let section2 = Section()
+        let row2_1 = Row()
+        let row2_2 = Row()
+        section2.addRows([row2_1, row2_2])
+        
+        source.addSections([section1, section2])
+        
+        XCTAssert(source.sectionWith(0) as! Section === section1)
+        XCTAssert(source.sectionWith(1) as! Section === section2)
+        XCTAssert(source.rowWith(NSIndexPath(forRow: 0, inSection: 0)) as! Row === row1_1)
+        XCTAssert(source.rowWith(NSIndexPath(forRow: 1, inSection: 0)) as! Row === row1_2)
+        XCTAssert(source.rowWith(NSIndexPath(forRow: 0, inSection: 1)) as! Row === row2_1)
+        XCTAssert(source.rowWith(NSIndexPath(forRow: 1, inSection: 1)) as! Row === row2_2)
     }
     
 }
