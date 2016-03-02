2de5edfef04be42e4df51a3f28e885272cad8cef
都築 貴之 <tsutaka@192.168.102.34>
2016-03-02 13:27:22 +0900

    change access control of init method.

--- Classes/Row.swift
+++ Classes/Row.swift
@@ -12,7 +12,7 @@ public class Row<T: UITableViewCell>: RowType {
     public typealias RowInformation = (row: Row<T>, tableView: UITableView, indexPath: NSIndexPath)
     public typealias RowMoveInformation = (row: Row<T>, tableView: UITableView, sourceIndexPath: NSIndexPath, destinationIndexPath: NSIndexPath)
     
-    init() { }
+    public init() { }
     
     init(@noescape closure: (Row<T> -> Void)) {
         closure(self)
