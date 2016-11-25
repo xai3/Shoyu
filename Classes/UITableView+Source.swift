7d498587d4a83d2534f246255b7a46b5a6bfece4
yukiasai <yukiasai@gmail.com>
2016-11-25 15:51:05 +0900

    Fix variable name to camel case

--- Classes/UITableView+Source.swift
+++ Classes/UITableView+Source.swift
@@ -10,18 +10,18 @@ import UIKit
 import ObjectiveC
 
 struct UITableViewAssociatedObjectHandle {
-    static var Source: UInt8 = 0
+    static var source: UInt8 = 0
 }
 
 public extension UITableView {
     public var source: Source? {
         set {
-            objc_setAssociatedObject(self, &UITableViewAssociatedObjectHandle.Source, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, &UITableViewAssociatedObjectHandle.source, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
             dataSource = newValue
             delegate = newValue
         }
         get {
-            return objc_getAssociatedObject(self, &UITableViewAssociatedObjectHandle.Source) as? Source
+            return objc_getAssociatedObject(self, &UITableViewAssociatedObjectHandle.source) as? Source
         }
     }
 }
